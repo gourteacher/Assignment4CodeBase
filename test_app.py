4ab59b9511ad8fbbb0ced18e080c8d260c33ab17
John Gebre <Johnnygebre@cmail.carleton.ca>
2021-12-03 23:41:35 -0500

    Failed run

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import app
 
 class TestApp():
 
-    def test_index(self):
+   def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         assert(response.status_code  == 200)
